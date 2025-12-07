939688133502b8fd3cec2836d3f3c4d810804d61
Dor Hakim <dorhakim100@gmail.com>
2025-12-07 19:55:21 +0200

    Update ExerciseInstructions model

--- src/types/Exercise/ExerciseInstructions.ts
+++ src/types/Exercise/ExerciseInstructions.ts
@@ -4,6 +4,5 @@ import { ExpectedActual } from '../ExpectedActual/ExpectedActual'
 export interface ExerciseInstructions {
   exerciseId: string
   workingSets: WorkingSet[]
-  rpe: ExpectedActual<number>
   notes: ExpectedActual<string>
 }
--- src/types/Exercise/WorkingSet.ts
+++ src/types/Exercise/WorkingSet.ts
@@ -3,4 +3,6 @@ import { ExpectedActual } from '../ExpectedActual/ExpectedActual'
 export interface WorkingSet {
   weight: ExpectedActual<number>
   reps: ExpectedActual<number>
+  rpe?: ExpectedActual<number>
+  rir?: ExpectedActual<number>
 }
